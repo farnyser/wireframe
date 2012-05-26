61bb1e0c783b40a9c0d6b8f6b23c4f20386cc22b
Joseph Silvestre <joseph.silvestre38@gmail.com>
2012-05-26 16:44:05 +0200

    Ajout des animations slide up / slide down

--- src/view/PageMenu.java
+++ src/view/PageMenu.java
@@ -7,6 +7,10 @@ import org.mt4j.input.inputProcessors.MTGestureEvent;
 import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragEvent;
 import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragProcessor;
 import org.mt4j.util.MTColor;
+import org.mt4j.util.animation.Animation;
+import org.mt4j.util.animation.AnimationEvent;
+import org.mt4j.util.animation.IAnimationListener;
+import org.mt4j.util.animation.MultiPurposeInterpolator;
 import org.mt4j.util.math.Vector3D;
 
 import processing.core.PApplet;
@@ -15,10 +19,14 @@ public class PageMenu extends MTClipRectangle {
 	
 	protected MTTextArea textArea;
 	protected PageMenuProperties properties;
+	protected PApplet applet;
+	
+	private boolean animationRunning = false; // for the slide down animation	
 
 	PageMenu(PApplet applet) {
 		super(applet, 0, 0, 0, 400, 25);
 
+		this.applet = applet;
 		textArea = new MTTextArea(applet);
 		properties = new PageMenuProperties(applet);
 
@@ -40,7 +48,22 @@ public class PageMenu extends MTClipRectangle {
 	}
 	
 	protected void initGesture() {
+
 		this.removeAllGestureEventListeners();
+		
+		final int duration = 200;
+		MultiPurposeInterpolator slideDownInterpolator = new MultiPurposeInterpolator(0, PageMenuProperties.HEIGHT_WHEN_OPENED, duration, 0.0f, 1.0f, 1);
+		final Animation slideDownAnimation = new Animation("Slide down anim", slideDownInterpolator, this.properties, 0);
+		slideDownAnimation.addAnimationListener(new IAnimationListener() {
+			
+			public void processAnimationEvent(AnimationEvent ae) {
+				PageMenu.this.properties.setHeightLocal(ae.getValue());
+				
+				if(ae.getId() == AnimationEvent.ANIMATION_ENDED) {
+					animationRunning = false;
+				}
+			}
+		});
 		
 		this.addGestureListener(DragProcessor.class, new IGestureEventListener()
 		{
@@ -54,9 +77,12 @@ public class PageMenu extends MTClipRectangle {
 					move.subtractLocal(PageMenu.this.getCenterPointGlobal());
 					
 					if(move.y > 200 && (Math.abs(move.x) <= 100)) {
-						PageMenu.this.properties.setVisible(true);
+					
+						if (!animationRunning){
+							animationRunning = true;
+							slideDownAnimation.start();
+						}
 					}
-
 				}
 				
 		        return false;
--- src/view/PageMenuProperties.java
+++ src/view/PageMenuProperties.java
@@ -7,6 +7,10 @@ import org.mt4j.input.inputProcessors.MTGestureEvent;
 import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragEvent;
 import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragProcessor;
 import org.mt4j.util.MTColor;
+import org.mt4j.util.animation.Animation;
+import org.mt4j.util.animation.AnimationEvent;
+import org.mt4j.util.animation.IAnimationListener;
+import org.mt4j.util.animation.MultiPurposeInterpolator;
 import org.mt4j.util.math.Vector3D;
 
 import processing.core.PApplet;
@@ -14,9 +18,13 @@ import processing.core.PApplet;
 public class PageMenuProperties extends MTClipRectangle {
 	
 	protected MTTextArea ta;
+	
+	final static public float HEIGHT_WHEN_OPENED = 200;
+	
+	private boolean animationRunning = false; // for the slide up animation	
 
 	PageMenuProperties(PApplet applet) {
-		super(applet, 0, 0, 0, 400, 200);
+		super(applet, 0, 0, 0, 400, 0);
 
 		ta = new MTTextArea(applet);
 		
@@ -28,14 +36,26 @@ public class PageMenuProperties extends MTClipRectangle {
 		this.setFillColor(MTColor.GRAY);
 		
 		ta.setText("Supprimer la page");
-		
-		this.setVisible(false);
-		
 	}
 	
 	protected void initGesture() {
 
-		this.removeAllGestureEventListeners();		
+		this.removeAllGestureEventListeners();
+		
+		final int duration = 200;
+		MultiPurposeInterpolator slideUpInterpolator = new MultiPurposeInterpolator(PageMenuProperties.HEIGHT_WHEN_OPENED, 0, duration, 0.0f, 1.0f, 1);
+		final Animation slideUpAnimation = new Animation("Slide up anim", slideUpInterpolator, this, 0);
+		slideUpAnimation.addAnimationListener(new IAnimationListener() {
+			
+			public void processAnimationEvent(AnimationEvent ae) {
+				PageMenuProperties.this.setHeightLocal(ae.getValue());
+				
+				if(ae.getId() == AnimationEvent.ANIMATION_ENDED) {
+					animationRunning = false;
+				}
+			}
+		});
+		
 		this.addGestureListener(DragProcessor.class, new IGestureEventListener()
 		{
 			public boolean processGestureEvent(MTGestureEvent ge) 
@@ -45,9 +65,11 @@ public class PageMenuProperties extends MTClipRectangle {
 					
 					Vector3D move = PageMenuProperties.this.getCenterPointGlobal();
 					move.subtractLocal(de.getTo());
-					System.out.println(move.y);
 					if(move.y > 100 && (Math.abs(move.x) <= 100)) {
-						PageMenuProperties.this.setVisible(false);
+						if (!animationRunning){
+							animationRunning = true;
+							slideUpAnimation.start();
+						}
 					}
 
 				}
